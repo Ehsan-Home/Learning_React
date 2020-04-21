2faf3bf89ac5cf180c9c2faeee0089207053161d
ehsan.ghasaei@gmail.com <ehsan.ghasaei@gmail.com>
2020-04-21 13:20:49 -0700

    HOOK overview

--- src/App.js
+++ src/App.js
@@ -1,61 +1,121 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Tree from "./Tree/Tree"
 
 
-class App extends Component {
+// class App extends Component {
 
-  state = {
-    Tree1 : {
-      name : "Chenar",
-      height : "600cm",
-    },
-    Tree2 : {
-      name : "Kaj",
-      height : "520cm",
-    },
-    Tree3 : {
-      name : "Sarvenaz",
-      height : "720cm",
-    },
-  }
+//   state = {
+//     Tree1 : {
+//       name : "Chenar",
+//       height : "600cm",
+//     },
+//     Tree2 : {
+//       name : "Kaj",
+//       height : "520cm",
+//     },
+//     Tree3 : {
+//       name : "Sarvenaz",
+//       height : "720cm",
+//     },
+//   }
   
-  switchStateHandler = () => {
-    console.log("in handler")
-    this.setState(
-      {
-        Tree2 : {
-          name: "CHHHH",
-          height : "900cm",
-        },
-        Tree3 : {
-          name:'Ehsan',
-          height : "1000",
-        },
-        Tree4 : {
-          name:'asghar',
-          height : "200",
-        },
-      }
-      )
-  }
+//   switchStateHandler = () => {
+//     console.log("in handler")
+//     this.setState(
+//       {
+//         Tree2 : {
+//           name: "CHHHH",
+//           height : "900cm",
+//         },
+//         Tree3 : {
+//           name:'Ehsan',
+//           height : "1000",
+//         },
+//         Tree4 : {
+//           name:'asghar',
+//           height : "200",
+//         },
+//       }
+//       )
+//   }
 
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi. This is Ehsan from react.js :D</h1>
-        <button onClick={this.switchStateHandler}>Submit</button>
-        <Tree name={this.state.Tree1.name} Height={this.state.Tree1.height}/>
-        <Tree name={this.state.Tree2.name} Height={this.state.Tree2.height}/>
-        <Tree name={this.state.Tree3.name} Height={this.state.Tree3.height}>Fruite:not clear</Tree>
-      </div>
-      // React.createElement('div' , {className:'App'} , 
-      //   React.createElement('h1' , null , 'Hi. This is Ehsan from react.js :DDDD'))
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hi. This is Ehsan from react.js :D</h1>
+//         <button onClick={this.switchStateHandler}>Submit</button>
+//         <Tree name={this.state.Tree1.name} Height={this.state.Tree1.height}/>
+//         <Tree name={this.state.Tree2.name} Height={this.state.Tree2.height}/>
+//         <Tree name={this.state.Tree3.name} Height={this.state.Tree3.height}>Fruite:not clear</Tree>
+//       </div>
+//       // React.createElement('div' , {className:'App'} , 
+//       //   React.createElement('h1' , null , 'Hi. This is Ehsan from react.js :DDDD'))
 
       
-    );
-  }
+//     );
+//   }
+// }
+
+// export default App;
+
+
+const app = (props) => {
+  const [initialState, newState] = useState(
+    {
+      Tree1 : {
+        name : "Chenar",
+        height : "600cm",
+      },
+      Tree2 : {
+        name : "Kaj",
+        height : "520cm",
+      },
+      Tree3 : {
+        name : "Sarvenaz",
+        height : "720cm",
+      },
+      //Letter : "some letter",
+    },
+    
+  );
+
+  const [Letter , setLetter] = useState("some letter 2");
+
+  console.log(initialState);
+
+  const switchStateHandler = () => {
+        console.log("in handler")
+        newState(
+          {
+            Tree1 : {
+              name: "CHHHH",
+              height : "900cm",
+            },
+            Tree2 : {
+              name:'Ehsan',
+              height : "1000",
+            },
+            Tree3 : {
+              name:'asghar',
+              height : "200",
+            },
+            //Letter : "some letter",
+          }
+          )
+      }
+
+
+  return (
+          <div className="App">
+            <h1>Hi. This is Ehsan from react.js :D</h1>
+            <button onClick={switchStateHandler}>Submit</button>
+            <Tree name={initialState.Tree1.name} Height={initialState.Tree1.height}/>
+            <Tree name={initialState.Tree2.name} Height={initialState.Tree2.height}/>
+            <Tree name={initialState.Tree3.name} Height={initialState.Tree3.height}>Fruite:not clear</Tree>
+          </div>
+  )
 }
 
-export default App;
+export default app;
