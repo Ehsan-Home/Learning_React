3777350ac0a5ae43231921cdca548b26b842d865
ehsan.ghasaei@gmail.com <ehsan.ghasaei@gmail.com>
2020-04-21 11:17:53 -0700

    Working with State

--- src/App.js
+++ src/App.js
@@ -6,14 +6,30 @@ let phase = 0;
 
 
 class App extends Component {
+
+  state = {
+    Tree1 : {
+      name : "Chenar",
+      height : "600cm",
+    },
+    Tree2 : {
+      name : "Kaj",
+      height : "520cm",
+    },
+    Tree3 : {
+      name : "Sarvenaz",
+      height : "720cm",
+    },
+  }
   
   render() {
     return (
       <div className="App">
         <h1>Hi. This is Ehsan from react.js :D</h1>
-        <Tree />
-        <Tree />
-        <Tree />
+        <button>Submit</button>
+        <Tree name={this.state.Tree1.name} Height={this.state.Tree1.height}/>
+        <Tree name={this.state.Tree2.name} Height={this.state.Tree2.height}/>
+        <Tree name={this.state.Tree3.name} Height={this.state.Tree3.height}>Fruite:not clear</Tree>
       </div>
       // React.createElement('div' , {className:'App'} , 
       //   React.createElement('h1' , null , 'Hi. This is Ehsan from react.js :DDDD'))
--- src/Tree/Tree.js
+++ src/Tree/Tree.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const tree = () => {
-    return <p>This is a tree</p>
+const tree = (props) => {
+    return (
+        <div>
+            <p>This is a {props.name} with height of {props.Height} </p>
+            <p>{props.children}</p>
+        </div>
+    )
 }
 
 export default tree;
