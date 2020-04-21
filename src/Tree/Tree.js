850bcb36d7687060d1fdd8af6ac0cdb28d6b8758
ehsan.ghasaei@gmail.com <ehsan.ghasaei@gmail.com>
2020-04-21 13:53:13 -0700

    Passing function to child component & bind method

--- src/Tree/Tree.js
+++ src/Tree/Tree.js
@@ -3,7 +3,7 @@ import React from 'react';
 const tree = (props) => {
     return (
         <div>
-            <p>This is a {props.name} with height of {props.Height} </p>
+            <p onClick={props.click}>This is a {props.name} with height of {props.Height} </p>
             <p>{props.children}</p>
         </div>
     )
